847831501e958e3931f1ae6c9031cb636cd31c76
veter-art <80009607+veter-art@users.noreply.github.com>
2022-03-21 17:38:31 +0300

    hw3 done
    added directive for changing attr

--- mapnote/src/app/app.module.ts
+++ mapnote/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { AppDirectiveDirective } from './app-directive.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    AppDirectiveDirective
   ],
   imports: [
     BrowserModule
