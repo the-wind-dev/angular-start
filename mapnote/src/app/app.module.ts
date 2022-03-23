93abbde277b9681e24f0f74c8c1278dcf5627aab
veter-art <80009607+the-wind-dev@users.noreply.github.com>
2022-03-23 22:40:51 +0300

    hw4 done
    added pipe to format currency

--- mapnote/src/app/app.module.ts
+++ mapnote/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { AppDirectiveDirective } from './app-directive.directive';
+import { CurrencyPipe } from './currency.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    AppDirectiveDirective
+    AppDirectiveDirective,
+    CurrencyPipe
   ],
   imports: [
     BrowserModule
