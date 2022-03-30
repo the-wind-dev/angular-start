f52cdbf69191f83ca998a6151c9ac3521e23d036
the-wind-dev <80009607+the-wind-dev@users.noreply.github.com>
2022-03-30 22:40:59 +0300

    some fixes for hw6

--- mapnote/src/app/product-form/product-form.component.ts
+++ mapnote/src/app/product-form/product-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormControl, FormArray, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,34 +7,61 @@ import { FormControl, FormArray, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./product-form.component.scss']
 })
 export class ProductFormComponent{
+
+  formId = '10';
+
+  product: Product = {
+    name: 'default name',
+    price: null,
+    id: 'default id',
+    categories: [],
+  }
   
-  formId: string = '10';
 
   productFormGroup = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(4)] ),
-    price: new FormControl(null, [Validators.required, Validators.min(0) ]),
-    id: new FormControl(this.formId, Validators.required),
+    name: new FormControl(this.product.name, [Validators.required, Validators.minLength(4)] ),
+    price: new FormControl(this.product.price, [Validators.required, Validators.min(0) ]),
+    id: new FormControl(this.product.id, Validators.required),
     categories: new FormControl([]),
   })
 
+  
+  refreshProduct() {
+    for (let key in this.product) {
+      this.product[key] =  this.productFormGroup.controls[key].value;
+    }
+  }
 
   onSubmit() {
+    this.refreshProduct();
     if (this.productFormGroup.invalid) {
       alert('form is invalid')
+    } else {
+      
+      console.log(this.product);
     }
-    console.log(this.productFormGroup.controls);
+    
+    // this.makeProduct();
+    console.log(this.product);
+    // console.log(this.productFormGroup.controls);
+    
 
   }
-  generateID(): void {
-    this.formId = Math.ceil(Math.random() * 100).toString();
-    console.log(this.formId);
+  generateID() {
+    this.product.id = Math.ceil(Math.random() * 100).toString();
+    // this.formId = Math.ceil(Math.random() * 100).toString();
+
+    // this.refreshProduct();
+    console.log(this.product.id);
+    // console.log(this.productFormGroup.controls.id.value);
+    // console.log(this.product);
   }
   
 }
 
-// interface Product {
-//   name: string,
-//   price: number,
-//   id: string,
-//   categories: string[],
-// }
+interface Product {
+  name: string,
+  price: number,
+  id: string,
+  categories: string[],
+}
